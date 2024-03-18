788217be081ee9f9d798bb3e84a8d2ab2445b343
parasSolanki <parassolanki151@gmail.com>
2024-03-18 20:18:58 +0530

    add: scalar docs

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,6 +1,5 @@
 import { logger } from "hono/logger";
 import { prettyJSON } from "hono/pretty-json";
-import { swaggerUI } from "@hono/swagger-ui";
 import {
   authRoutes,
   categoryRoutes,
@@ -9,18 +8,27 @@ import {
   storageRoutes,
 } from "./routes/index.js";
 import { createOpenApiHono } from "./utils/openapi-hono.js";
+import { apiReference } from "@scalar/hono-api-reference";
 
 export const app = createOpenApiHono()
   .doc31("/doc", {
     openapi: "3.1.0",
     info: {
       version: "1.0.0",
-      title: "API",
+      title: "Tetoy API Docs",
     },
   })
   .use("*", logger())
   .use("*", prettyJSON())
-  .get("/ui", swaggerUI({ url: "/doc" }))
+  .get(
+    "/docs",
+    apiReference({
+      pageTitle: "Tetoy API Docs",
+      spec: {
+        url: "/doc",
+      },
+    })
+  )
   .route("/api/auth", authRoutes)
   .route("/api/me", meRoutes)
   .route("/api/categories", categoryRoutes)
